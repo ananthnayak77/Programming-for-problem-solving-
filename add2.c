a024bbe3ae22853fa64eb524d7a3dc0fa7cafb03
ananthnayak77 <ananthnayakmuniyal@gmail.com>
2026-08-20 00:09:10 +0530

    addition

---
+++ add2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter the number1 =\n",a);
+    printf("enter the number2 =\n",b);
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("sum of the two given numbers is =%d",sum);
+    return 0;
+}
