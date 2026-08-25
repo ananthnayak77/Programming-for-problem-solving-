450a98f36a54fd7ca27c0399cc0a3bdee3c3f049
ananthnayak77 <ananthnayakmuniyal@gmail.com>
2026-08-25 19:38:21 +0530

    determines the bit size of the datatype

---
+++ sizeof.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{printf("size of char: %zu byte(s)\n",sizeof(char));
+printf("size of int: %zu byte(s)\n",sizeof(int));
+printf("size of float: %zu byte(s)\n",sizeof(float));
+printf("size of double: %zu byte(s)\n",sizeof(double));
+printf("size of long int: %zu byte(s)\n",sizeof(long int));
+printf("size of long double: %zu byte(s)\n",sizeof(long double));
+return 0;
+}
