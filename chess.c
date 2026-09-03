02a41e19ec8c554c03f4e52e21027731007e866c
ananthnayak77 <ananthnayakmuniyal@gmail.com>
2026-09-03 19:23:46 +0530

    login UI  interface in c

--- chess.c
+++ chess.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 
 // Board representation: Uppercase = White, Lowercase = Black, '.' = Empty
+
 char board[8][8] = {
     {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
     {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
